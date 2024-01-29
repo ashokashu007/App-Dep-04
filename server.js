7c02a407b02df4a9aa8eaa1034bf56d28f111bf8
AshokE <ashokemadi@gmail.com>
2024-01-29 19:20:32 +0530

    sever committed

--- server.js
+++ server.js
@@ -2,10 +2,12 @@ const mongoose = require("mongoose");
 const express = require("express");
 const cors = require("cors");
 const multer = require("multer");
+const path = require("node:path")
 
 const app = express();
 app.use(cors());
-app.use("/uploads",express.static("uploads"))
+app.use("/uploads",express.static("uploads"));
+app.use(express.static(path.join(__dirname,"./client/build")));
 
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
